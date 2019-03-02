a2094edf6b33b4511219f4c552b92a13707116d2
Mizael Sousa <aluno@MacBook-Pro-de-Aluno.local>
2019-03-01 20:25:28 -0400

    Botao de aumentar e diminuir valor

---
+++ SomaDiminuiApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SomaDiminuiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Aumentar(Object sender, EventArgs e)
+        {
+            int resultado = Int32.Parse(Result.Text) + 1;
+            Result.Text = resultado.ToString();
+        }
+
+        void Diminuir(Object sender, EventArgs e)
+        {
+            int resultado = Int32.Parse(Result.Text) - 1;
+            Result.Text = resultado.ToString();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
